0ebc474a015417a60ff48db70f7fceb769627f82
DassiErenshtein <hadasa2115@gmail.com>
2025-06-24 02:19:22 +0300

    add good server url

--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7286/api/'
+  // apiUrl: 'https://localhost:7286/api/'
+  apiUrl:'https://patio-furniture.onrender.com/api/'
 };
